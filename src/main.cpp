a87fd323cfd18d1d73661ee304f35d15fe4e784c
saliannazzo <saliannazzo06@gmail.com>
2026-05-07 16:24:32 -0500

    added rove comm stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,44 @@
 #include <Arduino.h>
-
-const int EN = 5;
-const int M0 = 6;
-const int M1 = 7;
-const int M2 = 8;
-const int STDBY = 9;
-const int STEP = 1;
-const int DIR = 11;
+#include "RoveComm.h"
+uint16_t data[2048]{};
+const int EN = 41;
+const int M0 = 0;
+const int M1 = 1;
+const int M2 = 2;
+const int STDBY = 38;
+const int STEP = 39;
+const int DIR = 40;
 const int LIMIT_SWITCH = 27;
 int dir = 0;
 int m0 = 1, m1 = 1, m2 = 1; // sets steps to 256 steps/rev.
 int count = 0;              // Initialize count variable
-float wavelength = 0.0;     // Initialize wavelength variable
 const int maxsteps = 256;
-float data[256];
+float Data[maxsteps]; // array that stores the wavelength values
+float initialWavelength = 0.0; // Initialize initial wavelength variable
+float wavelength = initialWavelength;     // Initialize wavelength variable
 /*
 TODO:
 Get the wavelength change per step and the initial wavelength.
+actually get the maximum steps needed
 */
+RoveCommEthernet roveComm;
+RoveCommPacket packet;
+
+void doRoveComm();
+void sendData();
 void zeroCount();
+#define LASER_OUT
 // put function declarations here:
 float findIPD(float VP_PD, float Vbias, float Rf);
 int myFunction(int x, int y);
 void clockwise();
 void counterClockwise();
 void printWaveLength(float *WaveLength);
-void step();
+void stepread();
 void setup()
 {
   Serial.begin(115200);
+  roveComm.begin(RC_RAMANBOARD_IPADDRESS);
   pinMode(STDBY, OUTPUT);
   pinMode(M0, OUTPUT);
   pinMode(M1, OUTPUT);
@@ -44,15 +54,15 @@ void setup()
   delay(1000); // Wait for the driver to initialize
 }
 
-void loop()
-{
+void loop(){
   // put your main code here, to run repeatedly:
+  doRoveComm();
   if (Serial.available() > 0)
   {
     count = Serial.parseInt();
   }
   counterClockwise();
-    stepread();
+  stepread();
   clockwise();
   stepread();
 }
@@ -69,6 +79,48 @@ void stepread()
     delay(100);
   }
 }
+void doRoveComm()
+{
+    //This will retrieve the most recent packet sent and pass it 
+    //into the "packet" variable.
+    roveComm.read(packet);
+
+    //This switch will just cover each different packet you could receive
+    switch(packet.dataId)
+    {
+    //Change Laser State
+    
+    //case RC_RAMANBOARD_LASER_DATA:
+        
+    //this sets the pin to the value that is passed through rovecomm
+        
+        //digitalWrite(LASER_OUT, packet.i8data[0]);
+        
+        //TODO: Watchdog stuff so the laser doesn't stay on 
+        //[i can help with this if you need]
+
+        break;
+
+    case RC_RAMANBOARD_REQUESTRAMANREADING_DATA_ID:
+        //This will be where you read your data and send it back
+        
+        sendData();
+
+        break;
+    }
+}
+void sendData()
+{
+    //This is where you send the data back to basestation.
+    //Because this is built for a different system it will be expecting
+    //an array of 2048 uint16_t's you do not have to use all of them.
+    roveComm.write(RC_RAMANBOARD_RAMANREADING_PART1_DATA_ID, 512, &data[0]);
+        roveComm.write(RC_RAMANBOARD_RAMANREADING_PART2_DATA_ID, 512, &data[512]);
+        roveComm.write(RC_RAMANBOARD_RAMANREADING_PART3_DATA_ID, 512, &data[1024]);
+        roveComm.write(RC_RAMANBOARD_RAMANREADING_PART4_DATA_ID, 512, &data[1536]);
+    //You have to send the data in 4 parts.
+}
+
 int myFunction(int x, int y)
 {
   return x + y;
@@ -104,5 +156,6 @@ void zeroCount()
     digitalWrite(STEP, LOW);
     delay(100);
   }
+ wavelength = initialWavelength;
   count = 0;
 }
